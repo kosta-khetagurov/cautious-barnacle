de83ef015b4d9ebe87e5fed67399cba0bf6cca57
kosta_kh <khetagurov235@gmail.com>
2021-04-26 09:52:00 +0300

    run instructions

--- cautious-barnacleTests/WordsProviderTests.swift
+++ cautious-barnacleTests/WordsProviderTests.swift
@@ -1,14 +1,13 @@
 //
-//  cautious_barnacleTests.swift
+//  WordsProviderTests.swift
 //  cautious-barnacleTests
 //
-//  Created by Konstantin Khetagurov on 16.04.2021.
+//  Created by Konstantin Khetagurov on 24.04.2021.
 //
 
 import XCTest
-@testable import cautious_barnacle
 
-class cautious_barnacleTests: XCTestCase {
+class WordsProviderTests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
---
+++ cautious-barnacleTests/WordsServiceMock.swift
@@ -0,0 +1,8 @@
+//
+//  WordsServiceMock.swift
+//  cautious-barnacleTests
+//
+//  Created by Konstantin Khetagurov on 24.04.2021.
+//
+
+import Foundation
